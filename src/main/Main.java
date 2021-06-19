d9f55e4b26ebc82638d8376e6323a105a9fab0ca
tulshidas <bsse0811@iit.du.ac.bd>
2021-06-19 10:27:46 +0100

    switch to openjdk

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gitstudio;
+package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
