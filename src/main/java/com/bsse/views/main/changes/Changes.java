5e279b751aef69b96ab7dfa485b2f48584d4d366
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-02 19:45:37 +0000

    make action options

--- src/main/java/com/bsse/views/main/changes/Changes.java
+++ src/main/java/com/bsse/views/main/changes/Changes.java
@@ -1,15 +1,15 @@
 package com.bsse.views.main.changes;
 
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 public class Changes extends VBox{
+	private final ChangesActionTab actionBar = new ChangesActionTab();
 	public Changes() {
 		super();
 		addChilds();
 	}
 	private void addChilds() {
-		var text = new Text("Selected Changes");
-		this.getChildren().addAll(text);
+		//var text = new Text("Selected Changes");
+		this.getChildren().addAll(actionBar);
 	}
 }
