7b8cd0211863777a54e82c34cbb6b689a12a1560
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-04 18:46:00 +0000

    track new files

---
+++ src/main/java/com/bsse/views/icons/MinusIcon.java
@@ -0,0 +1,14 @@
+package com.bsse.views.icons;
+
+import javafx.scene.shape.SVGPath;
+
+public class MinusIcon extends SVGPath{
+	public MinusIcon() {
+		super();
+		setStyle();
+	}
+	
+	private void setStyle(){
+		
+	}
+}
