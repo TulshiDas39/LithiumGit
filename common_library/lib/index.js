81c84067bacef5558ef8c55b7bb6a5db24f8ba13
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-11 14:37:30 +0000

    track files

---
+++ common_library/lib/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayGoodbye = exports.sayHello = void 0;
+var hello_world_1 = require("./hello-world");
+Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return hello_world_1.sayHello; } });
+Object.defineProperty(exports, "sayGoodbye", { enumerable: true, get: function () { return hello_world_1.sayGoodbye; } });
