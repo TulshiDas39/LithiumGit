2501eb918f101b10b3394b51f773a6f4de2c4151
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-15 15:23:58 +0000

    spacing fix

--- frontend/src/components/repositorySelection/repoSelectionPanels/recentRepositoryPanel/SelectedRecentRepoProperties.tsx
+++ frontend/src/components/repositorySelection/repoSelectionPanels/recentRepositoryPanel/SelectedRecentRepoProperties.tsx
@@ -21,10 +21,13 @@ function SelectedRecentRepoPropertiesComponent(props:ISelectedRecentRepoProperti
     return <div className="w-25 d-flex flex-column ps-2">
         <h3 className="m-0 py-2">{props.selectedItem.name}</h3>
         <hr className="m-0" />
-        <div className="py-2">
-            <Button className="px-5" onClick={handleOpen}>Open</Button>
-            <span className="px-1" />
-            <Button className="px" onClick={handleOpenInExplorer}>Open in explorer</Button>
+        <div className="py-2 d-flex justify-content-around flex-wrap">
+            <div className="py-1">
+                <Button className="px-5" onClick={handleOpen}>Open</Button>
+            </div>
+            <div className="py-1">
+                <Button className="px" onClick={handleOpenInExplorer}>Open in explorer</Button>
+            </div>
             
         </div>
         {!!props.selectedItem.lastOpenedAt &&
