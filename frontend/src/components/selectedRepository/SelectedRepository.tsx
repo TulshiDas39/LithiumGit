7209e45d996c22dfaa4e9ee0733bcb72af317133
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-15 18:49:44 +0000

    create selected repo left panel

--- frontend/src/components/selectedRepository/SelectedRepository.tsx
+++ frontend/src/components/selectedRepository/SelectedRepository.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useMultiState } from "../../lib";
+import { ISelectedRepoTabItem, SelectedRepoLeft } from "./SelectedRepoLeft";
+import './SelectedRepository.scss';
+
+interface IState{
+    selectedTab:ISelectedRepoTabItem;
+}
 
 function SelectedRepositoryComponent(){
-    return <div>SelectedRepositoryView</div>
+    const[state,setState]=useMultiState<IState>({selectedTab:{text:"Branch Explorer",type:"Branch_Explorer"}});
+    const handleSelect = (tab:ISelectedRepoTabItem)=>{
+        setState({selectedTab:tab});
+    }
+    return <div id="SelectedRepository" className="d-flex h-100">
+        <SelectedRepoLeft  onSelectTab={handleSelect} selectedTab={state.selectedTab} />
+    </div>
 }
 
 export const SelectedRepository = React.memo(SelectedRepositoryComponent);
