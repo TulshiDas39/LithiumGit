4130643483b4efe5e1ace54a803624f673f83bf3
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-15 11:29:42 +0000

    set selected tab recent if recent opened repo exist

--- frontend/src/components/topNav/secondTopNav/SecondTopNav.tsx
+++ frontend/src/components/topNav/secondTopNav/SecondTopNav.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import {FaHome} from "react-icons/fa"
-import { shallowEqual, useDispatch } from "react-redux";
+import { batch, shallowEqual, useDispatch } from "react-redux";
 import { useSelectorTyped } from "../../../store/rootReducer";
 import { ActionSavedData } from "../../../store/slices";
+import { ActionUI, EnumHomePageTab } from "../../../store/slices/UiSlice";
 import { ActionBar } from "./ActionBar";
 
 function SecondTopNavComponent(){
     const store = useSelectorTyped(state=>({
         hasSelectedRepo:state.savedData.recentRepositories.some(x=>x.isSelected),
+        recentRepos:state.savedData.recentRepositories,
     }),shallowEqual);
     const dispatch = useDispatch();
     
     const handleHomeClick=()=>{
-        dispatch(ActionSavedData.deSelectRepo());
+        let selectedTab = EnumHomePageTab.Recent;
+        if(!store.recentRepos.length) selectedTab = EnumHomePageTab.Open;
+        batch(()=>{
+            dispatch(ActionSavedData.deSelectRepo());
+            dispatch(ActionUI.setHomePageTab(selectedTab));
+        });
+        
     }
 
     return <div className="d-flex bg-third-color py-2 align-items-center">
