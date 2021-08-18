b7bb15979dbb7cac899053f65a03a12ff0ec2411
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-18 17:14:22 +0000

    spacing

--- frontend/src/components/selectedRepository/selectedRepoRight/CommitProperty.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/CommitProperty.tsx
@@ -8,12 +8,12 @@ interface ICommitProperty{
 
 function CommitPropertyComponent(props:ICommitProperty){
     if(!props.selectedCommit) return null;
-    return <div id="commit_property" className="d-flex flex-column w-100 overflow-hidden border">
+    return <div id="commit_property" className="d-flex flex-column w-100 ps-1 overflow-hidden border">
         <h6>Commit properties</h6>
         <span>Sha: {props.selectedCommit.avrebHash}</span>
         <span>Date: {moment(props.selectedCommit.date).format("D MMM,YYYY") }</span>
         <span>Author: {props.selectedCommit.author_name}&lt;{props.selectedCommit.author_email}&gt;</span>
-        <div className="ps-5">
+        <div className="">
             <textarea name="message" rows={8} className="no-resize w-75" 
                 value={props.selectedCommit.message} />            
         </div>
