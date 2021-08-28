82886973ea683595baad45d2672ad9c3f22b106d
tulshi das <bsse0811@iit.du.ac.bd>
2021-08-28 17:28:18 +0000

    create resizable panel

--- frontend/src/components/selectedRepository/selectedRepoRight/changes/Changes.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/changes/Changes.tsx
@@ -1,8 +1,41 @@
-import React from "react"
+import React, { useRef } from "react"
+import { useMultiState } from "../../../../lib";
+
+interface IState{
+    adjustedX:number;
+}
 
 function ChangesComponent(){
-    return <div>
-        Changes
+    const [state,setState] = useMultiState<IState>({adjustedX:0})
+    const dragData = useRef({initialX:0,currentX:0});
+    // useEffect(()=>{
+    //     initialDragData.current.clientX
+    // },[])
+    const setAdjustedX=()=>{
+        setState({adjustedX:dragData.current.currentX - dragData.current.initialX});
+    }
+    const handleResize = (e: React.DragEvent<HTMLDivElement>)=>{
+        console.log(e);
+        if(dragData.current.initialX === 0) dragData.current.initialX = e.screenX;
+        if(e.screenX !== 0) dragData.current.currentX = e.screenX;
+        setAdjustedX();
+    }
+    const getAdjustedSize=(adjustedX:number)=>{
+        if(adjustedX > 0) return `+ ${adjustedX}px`;
+        return `- ${-adjustedX}px`;
+    }
+    console.log(dragData.current);
+
+    return <div className="d-flex w-100">
+        <div className="pe-2" style={{width:`calc(20% ${getAdjustedSize(state.adjustedX)})`}}>
+            Files
+        </div>
+        <div className="bg-info cur-resize" onDrag={handleResize} style={{width:'3px'}}>
+
+        </div>
+        <div className="ps-2" style={{width:`calc(80% - 3px ${getAdjustedSize(-state.adjustedX)})`}}>
+            Changes
+        </div>
     </div>
 }
 
