1d6283705f61e7ff8e9ddbb48d70af09badbff8f
tulshi das <bsse0811@iit.du.ac.bd>
2021-09-11 17:57:18 +0000

    prevent input text scroll

--- frontend/src/components/selectedRepository/selectedRepoRight/changes/Difference.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/changes/Difference.tsx
@@ -9,10 +9,11 @@ interface IDifference{
 
 interface IState{
     lines:string[];
+    rightEditorWidth:number;
 }
 
 function DifferenceComponent(props:IDifference){
-    const [state,setState]=useMultiState<IState>({lines:[]});
+    const [state,setState]=useMultiState<IState>({lines:[],rightEditorWidth:300});
 
     useEffect(()=>{
         if(props.path) window.ipcRenderer.send(RendererEvents.getFileContent().channel,props.path);
@@ -20,18 +21,37 @@ function DifferenceComponent(props:IDifference){
 
     useEffect(()=>{
         window.ipcRenderer.on(RendererEvents.getFileContent().replyChannel,(e,lines:string[])=>{
-            setState({lines:lines});
+            setState({lines:lines,rightEditorWidth:Math.max(...lines.map(l=>l.length))});            
         })
     },[])
     
     return <div className="d-flex w-100 h-100 overflow-auto">
-        <div  className="w-50 overflow-auto border-end d-flex flex-column" style={{whiteSpace: "pre"}} contentEditable={false}>              
+        <div  className="w-50 overflow-auto border-end" style={{whiteSpace: "pre"}}>
+            <div className="d-flex flex-column" style={{width:`${state.rightEditorWidth}ch`}}>
+                {
+                    state.lines.map((line,index)=> (
+                    <input 
+                        key={index} type="text" 
+                        value={line} className="outline-none" 
+                        onChange={e=>{e.preventDefault();e.stopPropagation(); return false;}}
+                        spellCheck={false} 
+                    />))
+                }
+            </div>
             {state.lines.map((line,index)=> (<div key={index}>{line}</div>))}
         </div>
-        <div className="w-50">
-            <div>
-
+        <div className="w-50 overflow-auto " >
+            <div className="d-flex flex-column" style={{width:`${state.rightEditorWidth}ch`}}>
+                {
+                    state.lines.map((line,index)=> (
+                        <input key={index} type="text" 
+                            value={line} className="outline-none"
+                            onChange={e=>{}}
+                            spellCheck={false}
+                        />))
+                }
             </div>
+            
         </div>
 
     </div>
