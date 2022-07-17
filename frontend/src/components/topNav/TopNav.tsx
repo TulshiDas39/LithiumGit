b9fd8c07c68c023250de77bf5ba3f1968ca9e29c
tulshi das <bsse0811@iit.du.ac.bd>
2022-07-17 06:14:31 +0000

    update layout

--- frontend/src/components/topNav/TopNav.tsx
+++ frontend/src/components/topNav/TopNav.tsx
@@ -1,14 +1,10 @@
 import React, { useRef } from "react";
-import { FirstTopNav } from "./FirstTopNav";
 import { SecondTopNav } from "./secondTopNav/SecondTopNav";
 
 function TopNavComponent(){
     const data = useRef({firstTopNavHeightPercent:30})
     return <div className="d-flex flex-column h-100 w-100">
-        <div style={{height:`${data.current.firstTopNavHeightPercent}%`}}>
-            <FirstTopNav />
-        </div>
-        <div style={{height:`${100 - data.current.firstTopNavHeightPercent}%`}}>
+        <div className="h-100">
             <SecondTopNav />
         </div>
     </div>
