fddfe098f51f12ccfd96fe814ba1bb8ec5437c58
tulshi das <bsse0811@iit.du.ac.bd>
2023-04-28 00:10:15 +0600

    implement dragging

--- frontend/src/components/selectedRepository/SelectedRepository.tsx
+++ frontend/src/components/selectedRepository/SelectedRepository.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useMultiState } from "../../lib";
+import React, { useMemo, useRef } from "react";
+import { useDrag, useMultiState } from "../../lib";
 import { SelectedRepoLeft } from "./SelectedRepoLeft";
 import { SelectedRepoRight2 } from "./selectedRepoRight/SelectedRepoRight2";
 import './SelectedRepository.scss';
@@ -10,19 +10,31 @@ interface ISelectedRepositoryProps{
 }
 
 interface IState{
-    leftWidth:number;
 }
 
 function SelectedRepositoryComponent(props:ISelectedRepositoryProps){
-    const[state,setState]=useMultiState<IState>({leftWidth:200});
-    // const handleSelect = (tab:ISelectedRepoTabItem)=>{
-    //     setState({selectedTab:tab});
-    // }
+    const[state,setState]=useMultiState<IState>({});
+    const leftWidthRef = useRef(200);
+    const positionRef = useRef(0);
+    const {currentMousePosition:position,elementRef:resizer} = useDrag();
+    console.log("position",position);
+    const leftWidth = useMemo(()=>{
+        if(!position){
+            leftWidthRef.current += positionRef.current;
+            return leftWidthRef.current;
+        }
+        positionRef.current = position.x;
+        return leftWidthRef.current + positionRef.current;
+    },[position?.x])
+
+    console.log("leftWidht",leftWidth);
+
     return <div id="SelectedRepository" className="d-flex h-100">
-        <div style={{width:`${state.leftWidth}px`}}>
+        <div style={{width:`${leftWidth - 3}px`}}>
             <SelectedRepoLeft height={props.height}  />
         </div>
-        <div style={{width:`calc(100% - ${state.leftWidth}px)`}}>
+        <div ref={resizer as any} className="bg-second-color cur-resize" style={{ width: '3px',zIndex:2 }} />
+        <div style={{width:`calc(100% - ${leftWidth}px)`}}>
             <SelectedRepoRight2 height={props.height} />
         </div>
     </div>
