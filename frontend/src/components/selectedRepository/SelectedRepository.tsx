428c1f506b64d142ed104f40edf9b7b324c7e881
tulshi das <bsse0811@iit.du.ac.bd>
2023-04-28 10:42:49 +0600

    fix resize issue

--- frontend/src/components/selectedRepository/SelectedRepository.tsx
+++ frontend/src/components/selectedRepository/SelectedRepository.tsx
@@ -17,24 +17,22 @@ function SelectedRepositoryComponent(props:ISelectedRepositoryProps){
     const leftWidthRef = useRef(200);
     const positionRef = useRef(0);
     const {currentMousePosition:position,elementRef:resizer} = useDrag();
-    console.log("position",position);
     const leftWidth = useMemo(()=>{
         if(!position){
             leftWidthRef.current += positionRef.current;
+            positionRef.current = 0;
             return leftWidthRef.current;
         }
         positionRef.current = position.x;
         return leftWidthRef.current + positionRef.current;
     },[position?.x])
 
-    console.log("leftWidht",leftWidth);
-
     return <div id="SelectedRepository" className="d-flex h-100">
         <div style={{width:`${leftWidth - 3}px`}}>
             <SelectedRepoLeft height={props.height}  />
         </div>
-        <div ref={resizer as any} className="bg-second-color cur-resize" style={{ width: '3px',zIndex:2 }} />
-        <div style={{width:`calc(100% - ${leftWidth}px)`}}>
+        <div ref={resizer as any} className="bg-second-color cur-resize" style={{ width: '3px' }} />
+        <div style={{width:`calc(100% - ${leftWidth}px)`}} className="overflow-hidden">
             <SelectedRepoRight2 height={props.height} />
         </div>
     </div>
