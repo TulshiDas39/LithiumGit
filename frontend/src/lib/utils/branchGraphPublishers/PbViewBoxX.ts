4434ef6eba9f5b3ec60432e63d8305f7c959ec46
tulshi das <tulshidas37@gmail.com>
2023-11-28 22:24:46 +0600

    fix horizontal viewboxX value processing

--- frontend/src/lib/utils/branchGraphPublishers/PbViewBoxX.ts
+++ frontend/src/lib/utils/branchGraphPublishers/PbViewBoxX.ts
@@ -6,15 +6,17 @@ export class PbViewBoxX extends DerivedState<number>{
     constructor(value:number){
         super(value);
         BranchGraphUtils.state.horizontalScrollRatio2.subscribe(this.update.bind(this));
+        BranchGraphUtils.state.zoomLabel2.subscribe(this.update.bind(this));
         BranchGraphUtils.state.panelWidth.subscribe(this.update.bind(this));
-        BranchGraphUtils.state.viewBoxWidth.subscribe(this.update.bind(this));
     }
-    protected getDerivedValue(): number {
+    protected getDerivedValue(): number {        
         const scrollRatio = BranchGraphUtils.state.horizontalScrollRatio2.value;
         const totalWidth = BranchUtils.repositoryDetails.branchPanelWidth;
-        const viewBoxWidth = BranchGraphUtils.state.viewBoxWidth.value;
-        const x = (totalWidth * scrollRatio) - (viewBoxWidth/2);
-        return x;
+        const zoomLabel = BranchGraphUtils.state.zoomLabel2.value;
+        const panelWidth = BranchGraphUtils.state.panelWidth.value;
+        const effectiveWidth = Math.max( totalWidth * zoomLabel,panelWidth);
+        const scrollableWidth = effectiveWidth - panelWidth;
+        return scrollRatio * scrollableWidth;
     }
     applyChange(): void {
         const svgElem = BranchGraphUtils.svgElement;
