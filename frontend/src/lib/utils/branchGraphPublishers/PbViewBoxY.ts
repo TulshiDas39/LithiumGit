5904180c8abc498e1b0be84f767f2a6c9eecff7e
tulshi das <tulshidas37@gmail.com>
2023-11-28 22:11:28 +0600

    fix horizonal scrolling issue

--- frontend/src/lib/utils/branchGraphPublishers/PbViewBoxY.ts
+++ frontend/src/lib/utils/branchGraphPublishers/PbViewBoxY.ts
@@ -6,15 +6,17 @@ export class PbViewBoxY extends DerivedState<number>{
     constructor(value:number){
         super(value);
         BranchGraphUtils.state.verticalScrollRatio2.subscribe(this.update.bind(this));
+        BranchGraphUtils.state.zoomLabel2.subscribe(this.update.bind(this));
         BranchGraphUtils.state.panelHeight.subscribe(this.update.bind(this));
-        BranchGraphUtils.state.viewBoxHeight.subscribe(this.update.bind(this));
     }
     protected getDerivedValue(): number {
         const scrollRatio = BranchGraphUtils.state.verticalScrollRatio2.value;
         const totalHeight = BranchUtils.repositoryDetails.branchPanelHeight;
-        const viewBoxHeight = BranchGraphUtils.state.viewBoxHeight.value;
-        const y = (totalHeight * scrollRatio) - (viewBoxHeight/2);
-        return y;
+        const zoomLabel = BranchGraphUtils.state.zoomLabel2.value;
+        const panelHeight = BranchGraphUtils.state.panelHeight.value;
+        const effectiveHeight = Math.max( totalHeight * zoomLabel,panelHeight);
+        const scrollableHeight = effectiveHeight - panelHeight;
+        return scrollRatio * scrollableHeight;        
     }
     protected applyChange(): void {
         const svgElem = BranchGraphUtils.svgElement;
