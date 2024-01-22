c094dd5c4f2e9594e8df1da5b89b1a311be80cba
tulshi das <tulshidas37@gmail.com>
2024-01-22 23:21:58 +0600

    remove auto staging option

--- frontend/src/components/selectedRepository/selectedRepoRight/changes/CommitBox.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/changes/CommitBox.tsx
@@ -4,19 +4,13 @@ import { Form } from "react-bootstrap";
 import { FaCheck } from "react-icons/fa";
 import { shallowEqual, useDispatch } from "react-redux";
 import { BranchUtils, UiUtils, useMultiState } from "../../../../lib";
-import { ActionSavedData } from "../../../../store";
 import { useSelectorTyped } from "../../../../store/rootReducer";
-
-interface ICommitBoxProps{
-    //onHeightChange:(height:number)=>void;
-}
 
 interface IState{
     value:string;
-    autoStatingEnabled:boolean;
 }
 
-function CommitBoxComponent(props:ICommitBoxProps){
+function CommitBoxComponent(){
     const store = useSelectorTyped(state=>({
         autoStagingEnabled:state.savedData.autoStagingEnabled,
         isMergingState:!!state.ui.status?.mergingCommitHash
@@ -36,23 +30,10 @@ function CommitBoxComponent(props:ICommitBoxProps){
 
     const ref = useRef<HTMLDivElement>();
 
-    useEffect(()=>{
-        // if(ref.current?.clientHeight)
-        //     props.onHeightChange(ref.current.clientHeight);
-
-    },[ref.current?.clientHeight])
-
     const [state,setState]= useMultiState({value:"",autoStatingEnabled:store.autoStagingEnabled} as IState);
-    useEffect(()=>{
-        setState({autoStatingEnabled:store.autoStagingEnabled});
-    },[store.autoStagingEnabled]);
 
     const handleCommit=()=>{
         window.ipcRenderer.send(RendererEvents.commit().channel,BranchUtils.repositoryDetails.repoInfo,state.value);
-    }
-    const handleAutoStageClick=()=>{
-        window.ipcRenderer.sendSync(RendererEvents.updateAutoStaging().channel,!state.autoStatingEnabled);                
-        dispatch(ActionSavedData.updateAutoStaging(!state.autoStatingEnabled));
     }
 
     useEffect(()=>{
@@ -63,7 +44,7 @@ function CommitBoxComponent(props:ICommitBoxProps){
 
     
 
-    return <div className="w-100" ref={ref as any}>
+    return <div className="w-100 pb-2" ref={ref as any}>
             <Form.Control as="textarea" rows={2} value={state.value} onChange={e=> setState({value:e.target.value})} onKeyUp={e=> {if (e.key === 'Enter' ) e.preventDefault(); }}        
                 type="textarea" className="w-100 rounded-0 no-resize" placeholder="Commit message" />
             <div className="row g-0 align-items-center pt-2 justify-content-center flex-nowrap overflow-hidden">  
@@ -79,14 +60,6 @@ function CommitBoxComponent(props:ICommitBoxProps){
                     </div>
                 </div>
                 <div className="col-3"></div>
-            </div>
-            <div className="row g-0 border-bottom pb-2 justify-content-center flex-nowrap overflow-hidden">
-                <div className="col-auto ps-1">
-                    <Form.Check id="auto_stage" checked={state.autoStatingEnabled} onChange={handleAutoStageClick} />
-                </div>
-                <div className="col-auto ps-1">
-                    <label htmlFor="auto_stage" className="small no-wrap">Automatically Stage all</label>
-                </div>
             </div>
     </div>
 }
