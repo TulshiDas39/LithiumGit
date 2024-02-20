ace83386e28d402045c7d5170015935d8322c909
tulshi das <tulshidas37@gmail.com>
2024-02-20 23:54:06 +0600

    update paginator ui

--- frontend/src/components/common/Pagination/Paginator.tsx
+++ frontend/src/components/common/Pagination/Paginator.tsx
@@ -28,14 +28,20 @@ function PaginatorComponent(props:IProps){
         props.onPageChange(props.pageIndex-1);
     }
 
-    return <div className="d-flex align-items-center">
-        <AppButton text="<<-First" type="default" onClick={handleFirst} />
-        <AppButton text="<-Prev" type="default" onClick={handlePrev} />
-        <div className="px-2">
-            <h5 style={{margin:0}}>{props.pageIndex+1}/{totalPage}</h5>
-        </div>        
-        <AppButton text="Next->" type="default" onClick={handleNext}/>
-        <AppButton text="Last->>" type="default" onClick={handleLast}/>
+    return <div className="d-flex align-items-center w-100 px-2">
+        <div className="flex-grow-1">
+            <span>Showing {props.pageIndex*props.pageSize +1} to {Math.min((props.pageIndex+1)*props.pageSize,props.total)}</span>
+        </div>
+        <div className="d-flex align-items-center">
+            <AppButton text="<<-First" type="default" onClick={handleFirst} disabled={props.pageIndex === 0} />
+            <AppButton text="<-Prev" type="default" onClick={handlePrev} disabled={props.pageIndex === 0} />
+            <div className="px-2">
+                <h5 style={{margin:0}}> Page {props.pageIndex+1}/{totalPage}</h5>
+            </div>        
+            <AppButton text="Next->" type="default" onClick={handleNext} disabled={props.pageIndex === totalPage-1}/>
+            <AppButton text="Last->>" type="default" onClick={handleLast} disabled={props.pageIndex === totalPage-1}/>
+        </div>
+        
     </div>
 }
 
