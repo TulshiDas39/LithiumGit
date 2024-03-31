8a191b4047d4d02a2f44e09d316f4f9a0be896ea
tulshi das <tulshidas37@gmail.com>
2024-03-31 22:11:28 +0600

    update fetch ui

--- frontend/src/components/topNav/secondTopNav/PullPushMenu.tsx
+++ frontend/src/components/topNav/secondTopNav/PullPushMenu.tsx
@@ -11,7 +11,9 @@ import { ActionModals } from "../../../store";
 
 interface IStatus{
     showPushTo:boolean;
-    showPullFrom:boolean;    
+    showPullFrom:boolean;
+    showFetchAll:boolean;
+    
 }
 
 function PullPushMenuComponent(){
@@ -22,7 +24,8 @@ function PullPushMenuComponent(){
         isDetached:!!state.ui.status?.isDetached
     }),shallowEqual);
 
-    const [state,setState] = useMultiState<IStatus>({showPushTo:false,showPullFrom:false});
+    const [state,setState] = useMultiState<IStatus>({showPushTo:false,
+        showPullFrom:false,showFetchAll:false});
 
     const dispatch = useDispatch();
     const currentText = useMemo(()=>{
@@ -70,10 +73,15 @@ function PullPushMenuComponent(){
         e.stopPropagation();
         setState({showPullFrom:!state.showPullFrom});
     }
+    const handleFetchCaretClick=(e: React.MouseEvent<SVGElement, MouseEvent>)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        setState({showFetchAll:!state.showFetchAll});
+    }
 
     useEffect(()=>{
         const handler = ()=>{
-            setState({showPushTo:false,showPullFrom:false})
+            setState({showPushTo:false,showPullFrom:false,showFetchAll:false})
         }
         document.addEventListener("click",handler);
 
@@ -140,20 +148,18 @@ function PullPushMenuComponent(){
                 </div>
             </div>
         </div>
-        <div className="col-auto ps-2 pe-1 cur-point">
-            <div className="row g-0 align-items-center h-100">
-                <div className="col-auto hover hover-bg-secondary border-end pe-2" onClick={_=> handleFetch(false)}>
-                    <FaArrowDown />
-                    <span>Fetch</span>
+
+        <div className="col-auto ps-1 pe-1">
+            <div className="row g-0 align-items-stretch h-100 bg-success">
+                <div className="col-auto d-flex align-items-center button-effect" onClick={_=> handleFetch(false)}>
+                    <span className="px-2 text-light">Fetch</span> 
                 </div>
-                <div className="col-auto">
-                    <Dropdown>
-                        <Dropdown.Toggle className="bg-transparent hover-bg-secondary text-dark border-0 outline-none outline-none-focus hover px-2">                            
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu className="min-w-auto py-0" align={"end"}>
-                            <Dropdown.Item className="" onClick={_=> handleFetch(true)}>Fetch All</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                <div className="border-secondary border-start border-end col-auto d-flex align-items-center position-relative">
+                    <FaCaretDown onClick={handleFetchCaretClick} />
+                    {state.showFetchAll && <div className="position-absolute bg-success py-1 px-2 button-effect" style={{top:'105%', right:0}}
+                        onClick={_=> handleFetch(true)}>
+                        <span className="text-nowrap text-light">Fetch all</span>
+                    </div>}
                 </div>
             </div>
         </div>
