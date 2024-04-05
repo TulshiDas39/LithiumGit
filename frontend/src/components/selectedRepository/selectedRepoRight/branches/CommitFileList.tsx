2f13b36c28b2308b61e0f465b50e12d71742a47c
tulshi das <tulshidas37@gmail.com>
2024-04-05 10:58:38 +0600

    strike through deleted item

--- frontend/src/components/selectedRepository/selectedRepoRight/branches/CommitFileList.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/branches/CommitFileList.tsx
@@ -1,4 +1,4 @@
-import { IFile, StringUtils } from "common_library";
+import { EnumChangeType, IFile, StringUtils } from "common_library";
 import React from "react"
 
 interface IProps{
@@ -11,7 +11,7 @@ function CommitFileListComponent(props:IProps){
         {
             props.files.map(f=>(
                 <div title={f.path} key={f.path} className="cur-default hover">
-                    <span>{f.fileName}</span> 
+                    <span className= {`${f.changeType === EnumChangeType.DELETED?"text-decoration-line-through":""}`}>{f.fileName}</span> 
                     <span className="ps-2 text-success fw-bold cur-default" title={StringUtils.getStatusText(f.changeType)}>{StringUtils.getChangeTypeHint(f.changeType)}</span>
                 </div>
             ))
