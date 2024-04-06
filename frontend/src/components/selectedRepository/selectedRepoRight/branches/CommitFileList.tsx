ed7cc30a6e24b0a7892d814d8429ee3d271e60bf
tulshi das <tulshidas37@gmail.com>
2024-04-06 10:10:07 +0600

    show path of files and add header to changed list of commit .

--- frontend/src/components/selectedRepository/selectedRepoRight/branches/CommitFileList.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/branches/CommitFileList.tsx
@@ -9,15 +9,29 @@ interface IProps{
 }
 
 function CommitFileListComponent(props:IProps){
-    return <div style={{width:props.width}} className="h-100 overflow-auto">
-        {
-            props.files.map(f=>(
-                <div title={f.path} key={f.path} className={`cur-default hover ${props.selectedFile?.path === f.path?'selected':''}`} onClick={_=> props.onFileSelect(f)}>
-                    <span className= {`${f.changeType === EnumChangeType.DELETED?"text-decoration-line-through":""}`}>{f.fileName}</span> 
-                    <span className="ps-2 text-success fw-bold cur-default" title={StringUtils.getStatusText(f.changeType)}>{StringUtils.getChangeTypeHint(f.changeType)}</span>
-                </div>
-            ))
-        }
+    return <div style={{width:props.width}} className="h-100">
+        <div style={{height:30}} className="ps-1">
+            <b>Changed Files</b>
+        </div>
+        <div className="overflow-auto ps-1" style={{height:`calc(100% - 30px)`}}>
+            {
+                props.files.map(f=>(
+                    <div title={f.path} key={f.path} className={`row g-0 align-items-center flex-nowrap hover w-100 cur-default ${props.selectedFile?.path === f.path?'selected':''}`} onClick={_=> props.onFileSelect(f)}>
+                        <div className={`col-auto overflow-hidden align-items-center flex-shrink-1`} style={{textOverflow:'ellipsis'}}>
+                            <span className= {`${f.changeType === EnumChangeType.DELETED?"text-decoration-line-through":""}`}>{f.fileName}</span> 
+                            <span className="small text-secondary">
+                                <span className="ps-1">{f.path}</span>
+                            </span>
+                        </div>
+                        <div className="col-auto align-items-center flex-nowrap overflow-hidden flex-grow-1 text-end">
+                            <span className="px-2 text-success fw-bold cur-default" title={StringUtils.getStatusText(f.changeType)}>{StringUtils.getChangeTypeHint(f.changeType)}</span>
+                        </div>
+                        
+                        {/* <span className="ps-2 text-success fw-bold cur-default" title={StringUtils.getStatusText(f.changeType)}>{StringUtils.getChangeTypeHint(f.changeType)}</span> */}
+                    </div>
+                ))
+            }
+        </div>        
     </div>
 }
 
