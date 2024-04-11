bee43de318bd763478c87ad269a3c278cfe88427
tulshi das <tulshidas37@gmail.com>
2024-04-12 00:01:43 +0600

    add hover effect to current box

--- frontend/src/components/topNav/secondTopNav/PullPushMenu.tsx
+++ frontend/src/components/topNav/secondTopNav/PullPushMenu.tsx
@@ -130,32 +130,34 @@ function PullPushMenuComponent(){
     }    
 
     return <div className="row g-0 align-items-stretch ps-2">
-        <div className="col-auto border px-1">
-            <div className="row g-0 align-items-center h-100">
-                <div className="col-auto">
-                    {currentText}
-                </div>
-                <div className="col-auto ps-1">
-                    <div className="row g-0 bg-info px-1 rounded">
-                        <div className="col-auto">
-                            <FaAngleDoubleUp />
-                        </div>
-                        <div className="col-auto">
-                            {store.ahead}
+        <div className="col-auto ">
+            <div className="d-flex justify-content-center h-100" style={{width:100 }}>
+                <div className="row g-0 align-items-center h-100 branchBox px-1">
+                    <div className="col-auto">
+                        {currentText}
+                    </div>
+                    <div className="col-auto ps-1">
+                        <div className="row g-0 bg-info px-1 rounded">
+                            <div className="col-auto">
+                                <FaAngleDoubleUp />
+                            </div>
+                            <div className="col-auto">
+                                {store.ahead}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-auto ps-1">
-                    <div className="row g-0 bg-info px-1 rounded">
-                        <div className="col-auto">
-                            <FaAngleDoubleDown />
-                        </div>
-                        <div className="col-auto">
-                            {store.behind}
+                    <div className="col-auto ps-1">
+                        <div className="row g-0 bg-info px-1 rounded">
+                            <div className="col-auto">
+                                <FaAngleDoubleDown />
+                            </div>
+                            <div className="col-auto">
+                                {store.behind}
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
 
         </div>
