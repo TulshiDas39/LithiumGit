a94e7272b1b0f7f1c1fe09df0b08a2a5cdb731e9
tulshi das <tulshidas37@gmail.com>
2024-04-15 22:55:50 +0600

    creating bottom panel

--- frontend/src/components/selectedRepository/selectedRepoRight/changes/ConflictBottomPanel.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/changes/ConflictBottomPanel.tsx
@@ -1,11 +1,43 @@
+import { Fragment } from "react";
 import { ILine } from "../../../../lib";
+
+interface ISingleLineProps{
+    line:ILine;
+    color:string;
+}
+
+function SingleLine(props:ISingleLineProps){
+    return <Fragment>
+        {props.isFirstConflict && 
+        <Fragment>
+        <p> 
+            <span className="hover">Accept Current Change</span> 
+            <span> |</span>
+            <span className="hover">Accept Incoming Change</span>
+            <span> |</span>
+            <span className="hover">Accept Both Change</span>
+        </p>
+        <p> &gt;&gt;&gt;&gt;&gt;&gt;&gt; HEAD (Current Changes)</p>
+        </Fragment>}
+        {!!props.currentLine.conflictNo &&
+            <Fragment>
+                <p>{}</p>
+            </Fragment>
+        }
+    </Fragment>
+}
 
 interface IProps{
     previousLines:ILine[];
     currentLines:ILine[];
 }
+
 export function ConflictBottomPanel(props:IProps){
     return <div className="h-100 w-100">
-
+        {
+            props.currentLines.map((line,ind)=>(
+                // <SingleLine line={} />
+            ))
+        }
     </div>
 }
