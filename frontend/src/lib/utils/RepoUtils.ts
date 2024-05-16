12343ee3e7ebdd9187b900a35b170b5bfb14555b
tulshi das <tulshidas37@gmail.com>
2024-05-16 20:15:41 +0600

    handle having only one branch

--- frontend/src/lib/utils/RepoUtils.ts
+++ frontend/src/lib/utils/RepoUtils.ts
@@ -66,13 +66,14 @@ export class RepoUtils{
         }
 
         const branches = repoDetails.resolvedBranches.filter(_=> !!_.parentCommit);
+        if(!!branches.length){
+            const maxOffset = ArrayUtils.findMax(branches.map(_=>_.verticalOffset));
 
-        const maxOffset = ArrayUtils.findMax(branches.map(_=>_.verticalOffset));
-
-        for(let offset = 2; offset <= maxOffset ; offset++){
-            const branchesOfThisOffset = branches.filter(_ => _.verticalOffset === offset);
-            branchesOfThisOffset.forEach(_ => setHeight(_));
-        }
+            for(let offset = 2; offset <= maxOffset ; offset++){
+                const branchesOfThisOffset = branches.filter(_ => _.verticalOffset === offset);
+                branchesOfThisOffset.forEach(_ => setHeight(_));
+            }   
+        }        
      
         repoDetails.branchPanelHeight = ArrayUtils.findMax(repoDetails.resolvedBranches.map(_=>_.y)) + 50;
     }
