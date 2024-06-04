4530df537eb1896aa28421a6e9b732700e94b2b7
tulshi das <tulshidas37@gmail.com>
2024-06-04 22:06:35 +0600

    move data path to home directory

--- src/dataClasses/AppData.ts
+++ src/dataClasses/AppData.ts
@@ -1,6 +1,9 @@
 import { app, BrowserWindow } from "electron";
+import path = require("path");
 
 export class AppData{
     static appPath:string = app.getAppPath();
+    private static homePath = app.getPath('home');
+    static dataPath = path.join(AppData.homePath,".lithiumgit");
     static mainWindow:BrowserWindow;
 }
