33f4820307302679bef9249c96bed6512815a9e0
tulshi das <tulshidas37@gmail.com>
2024-07-22 12:22:48 +0600

    move commit fetching to commitList component

--- frontend/src/components/selectedRepository/selectedRepoRight/commits/CommitList.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/commits/CommitList.tsx
@@ -1,35 +1,82 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { SingleCommit } from "./SingleCommit";
-import { ICommitInfo } from "common_library";
+import { ICommitInfo, ILogFilterOptions } from "common_library";
 import { Paginator } from "../../../common";
+import { RepoUtils, useMultiState } from "../../../../lib";
+import { IpcUtils } from "../../../../lib/utils/IpcUtils";
+import { useSelectorTyped } from "../../../../store/rootReducer";
+import { shallowEqual } from "react-redux";
 
 interface IProps{
-    loading:boolean;
-    commits:ICommitInfo[];
-    total:number;
+    searchText:string;
+    selectedBranch?:string;
+}
+
+interface IState{
     pageIndex:number;
     pageSize:number;
-    onPageChange:(pageIndex:number)=>void;
+    commits:ICommitInfo[];
+    loading:boolean;
+    total:number;
+    refreshKey:string;
 }
 
 function CommitListComponent(props:IProps){
+
+    const store = useSelectorTyped(state=>({
+        repo:state.savedData.recentRepositories.find(_=>_.isSelected)?.path,
+    }),shallowEqual);
+
+    const [state,setState] = useMultiState<IState>({
+        pageIndex:0,
+        pageSize:500,
+        commits:[],
+        loading:false,
+        total:0,
+        refreshKey:new Date().toISOString(),
+    });
+
+    useEffect(()=>{
+        const filterOptions:ILogFilterOptions = {
+            pageIndex:state.pageIndex,
+            pageSize:state.pageSize,            
+        }
+        if(props.searchText){
+            filterOptions.message = props.searchText;
+        }
+        if(props.selectedBranch){
+            filterOptions.branchName = props.selectedBranch;
+        }
+        
+        IpcUtils.getCommitList(filterOptions).then(result=>{
+            setState({commits:result.list.reverse(),total:result.count,loading:false});
+        });
+        
+    },[state.pageIndex,state.pageSize,props.searchText,props.selectedBranch,state.refreshKey]);
+
+    useEffect(()=>{
+        RepoUtils.enSureUpdate(store.repo!).then((r)=>{
+            setState({pageIndex:0,refreshKey:new Date().toISOString()});
+        })
+    },[store.repo])
+
     return <Fragment>
         <div className="w-100 overflow-auto d-flex justify-content-center align-items-start" style={{height:'80%'}}>
-            {props.loading && <div className="w-100 d-flex justify-content-center">
+            {state.loading && <div className="w-100 d-flex justify-content-center">
                 <span>Loading...</span> 
             </div>
             }
-            {!props.loading && <div className="w-100 px-2">
+            {!state.loading && <div className="w-100 px-2">
                 {
-                    props.commits.map(commit=>(
+                    state.commits.map(commit=>(
                         <SingleCommit key={commit.avrebHash} commit={commit} />
                     ))
                 }
             </div> }           
         </div>
         <div className="pt-2 d-flex justify-content-center align-items-start" style={{height:'10%'}}>
-            <Paginator total={props.total} pageIndex={props.pageIndex} pageSize={props.pageSize}
-                onPageChange={(pageIndex) => props.onPageChange(pageIndex)} />
+            <Paginator total={state.total} pageIndex={state.pageIndex} pageSize={state.pageSize}
+                onPageChange={(pageIndex) => setState({pageIndex})} />
         </div>
     </Fragment>
 }
--- frontend/src/components/selectedRepository/selectedRepoRight/commits/Commits.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/commits/Commits.tsx
@@ -9,87 +9,16 @@ import { useSelectorTyped } from "../../../../store/rootReducer";
 import { shallowEqual } from "react-redux";
 import { CommitList } from "./CommitList";
 
-
-interface ISingleCommitProps{
-    commit:ICommitInfo;
-}
-
-function SingleCommit(props:ISingleCommitProps){
-    const getTimeZonOffsetStr = ()=>{
-        return UiUtils.getTimeZonOffsetStr();
-    }
-    return <div className="py-1 w-100 overflow-auto">
-     <div className="border border-primary ps-2">
-        <div>
-            <span>Sha: </span>
-            <span>{props.commit.hash}</span>
-            {!!props.commit.refs && 
-             <b className="text-danger"> ({props.commit.refs})</b>}
-        </div>
-        <div>
-            <span>Date: </span>
-            <span title={getTimeZonOffsetStr()}>{moment(props.commit.date).format("MMMM Do YYYY, h:mm:ss a") }</span>
-        </div>
-        <div>
-            <span>Author: </span>
-            <span>{props.commit.author_name}({props.commit.author_email})</span>
-        </div>
-        <div>
-            <span>Message: </span>
-            <span>{props.commit.message}</span>
-        </div>
-    </div>
-    </div>
-}
-
 interface IState{
-    total:number;
-    pageIndex:number;
-    pageSize:number;
-    commits:ICommitInfo[];
-    loading:boolean;
     searchText:string;
     selectedBranch?:string;
-    refreshKey:string;
 }
 
-function CommitsComponent(){
-    const store = useSelectorTyped(state=>({
-        repo:state.savedData.recentRepositories.find(_=>_.isSelected)?.path,
-    }),shallowEqual);
+function CommitsComponent(){    
 
-    const [state,setState]=useMultiState<IState>({pageIndex:0,
-        pageSize:500,
-        total:0,
-        commits:[],
-        loading:true,
+    const [state,setState]=useMultiState<IState>({                
         searchText:"",
-        refreshKey: new Date().toISOString(),
-    });
-
-    useEffect(()=>{
-        const filterOptions:ILogFilterOptions = {
-            pageIndex:state.pageIndex,
-            pageSize:state.pageSize,            
-        }
-        if(state.searchText){
-            filterOptions.message = state.searchText;
-        }
-        if(state.selectedBranch){
-            filterOptions.branchName = state.selectedBranch;
-        }
-        
-        IpcUtils.getCommitList(filterOptions).then(result=>{
-            setState({commits:result.list.reverse(),total:result.count,loading:false});
-        });
-        
-    },[state.pageIndex,state.pageSize,state.searchText,state.selectedBranch,state.refreshKey]);
-
-    useEffect(()=>{
-        RepoUtils.enSureUpdate(store.repo!).then((r)=>{
-            setState({pageIndex:0,refreshKey:new Date().toISOString()});
-        })
-    },[store.repo])
+    });   
 
     const handleSearch = (text:string)=>{
         setState({searchText:text});
@@ -99,8 +28,7 @@ function CommitsComponent(){
         <div className="w-100" style={{height:'10%'}}>
             <CommitFilter onSearch={handleSearch} onBranchSelect={br=>setState({selectedBranch:br})} />
         </div>
-        <CommitList commits={state.commits}  loading={state.loading} onPageChange={(pageIndex)=> setState({pageIndex})}
-        pageIndex={state.pageIndex} pageSize={state.pageSize} total={state.total} />        
+        <CommitList searchText={state.searchText} selectedBranch={state.selectedBranch} />        
         
     </div>
 }
