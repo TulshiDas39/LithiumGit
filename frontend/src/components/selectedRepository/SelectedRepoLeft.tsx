ac5d096244701b4329814652dee7016bb87e561e
tulshi das <tulshidas37@gmail.com>
2024-07-22 23:18:39 +0600

    add configuration menu

--- frontend/src/components/selectedRepository/SelectedRepoLeft.tsx
+++ frontend/src/components/selectedRepository/SelectedRepoLeft.tsx
@@ -4,11 +4,15 @@ import { shallowEqual, useDispatch } from "react-redux";
 import { EnumSelectedRepoTab } from "../../lib";
 import { useSelectorTyped } from "../../store/rootReducer";
 import { ActionUI } from "../../store/slices/UiSlice";
+import { FaCog } from "react-icons/fa";
 
 export interface ISelectedRepoTabItem{
     type:EnumSelectedRepoTab;
     text:string;
     marked?:boolean;
+    className?:string;
+    innerHtml?:JSX.Element;
+    title?:string;
 }
 
 function SelectedRepoLeftComponent(){
@@ -24,7 +28,7 @@ function SelectedRepoLeftComponent(){
             {text:"Graph",type:EnumSelectedRepoTab.GRAPH},
             {text:"Commits",type:EnumSelectedRepoTab.COMMITS},
             {text:"Stashes",type: EnumSelectedRepoTab.STASHES},
-            {text:"Remotes",type: EnumSelectedRepoTab.REMOTES},            
+            {text:"Remotes",type: EnumSelectedRepoTab.REMOTES},                       
         ];
         return items;
     },[store.hasChanges]);
@@ -32,14 +36,23 @@ function SelectedRepoLeftComponent(){
     return <div id="SelectedRepoLeft" className="d-flex w-100 flex-column bg-second-color h-100">  
             {
                 tabs.map(t=>(
-                    <span key={t.type} 
-                        className={`tabItem w-100 py-2 hover ps-1 border-bottom ${store.tab === t.type?"bg-select-color":""}`}
+                    <span key={t.type} title={t.title || ""}
+                        className={`tabItem w-100 py-2 hover ps-1 border-bottom 
+                            ${t.className ? t.className:""} ${store.tab === t.type?"bg-select-color":""}`}
                         onClick={()=> dispatch(ActionUI.setSelectedRepoTab(t.type))}>
                             <span className="">{t.text}</span>
+                            {!!t.innerHtml && t.innerHtml}
                             {!!t.marked && <span className="text-primary">*</span>}
                     </span>
                 ))
-            }            
+            }
+
+            <span className={`w-100 flex-grow-1 d-flex align-items-end justify-content-center`}>
+                <span className={`tabItem py-2 hover ps-1 border-bottom w-100 text-center ${store.tab === EnumSelectedRepoTab.CONFIG?"bg-select-color":""}`} title="Configuration"
+                onClick={()=> dispatch(ActionUI.setSelectedRepoTab(EnumSelectedRepoTab.CONFIG))}>
+                    <span style={{fontSize:'2.5em'}}><FaCog /></span>
+                </span>
+            </span>            
     </div>
 }
 
--- frontend/src/lib/enums/EnumSelectedRepoTab.ts
+++ frontend/src/lib/enums/EnumSelectedRepoTab.ts
@@ -4,4 +4,5 @@ export enum EnumSelectedRepoTab{
     COMMITS="COMMITS",
     REMOTES="REMOTES",
     STASHES="STASHES",
+    CONFIG="CONFIG",
 }
