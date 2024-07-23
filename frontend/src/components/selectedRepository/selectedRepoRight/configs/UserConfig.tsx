e0273321b818c41d803e16c0f85c5c014abc4547
tulshi das <tulshidas37@gmail.com>
2024-07-23 16:19:09 +0600

    show switch to global configs

--- frontend/src/components/selectedRepository/selectedRepoRight/configs/UserConfig.tsx
+++ frontend/src/components/selectedRepository/selectedRepoRight/configs/UserConfig.tsx
@@ -1,45 +1,66 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { IpcUtils } from "../../../../lib/utils/IpcUtils";
 import { IScopedValue, ObjectUtils, useMultiState } from "../../../../lib";
-import { IUserConfig } from "common_library";
+import { ITypedConfig, IUserConfig } from "common_library";
+import { Form } from "react-bootstrap";
 
 
+interface IRefData{
+    user?:ITypedConfig<IUserConfig>;
+}
 
-interface IState extends Record<keyof IUserConfig, IScopedValue<string>>{
-
+interface IState{
+    showingGlobal:boolean;
+    user?:ITypedConfig<IUserConfig>;
 }
 
 
 function UserConfigComponent(){
-    const initialState:IState = {email:{value:"", isGlobal:false},name:{value:"",isGlobal:false}}
+    const initialState:IState = {showingGlobal:false};
     const [state,setState] = useMultiState<IState>(initialState);
+
+    const refData = useRef<IRefData>({})
         
     useEffect(()=>{
         IpcUtils.getUserConfig().then(r=>{
             if(!r.error){
-                const mapped = new ObjectUtils().mapToScopedValue(r.result!);
-                setState({...mapped});
+                refData.current.user = r.result;
+                setState({user:r.result});
             }
         });
     },[])
 
-    const headerWidth = 100;
+    const getValue = (key:keyof IUserConfig)=>{
+        if(state.showingGlobal)
+            return state.user?.global[key];
+        return state.user?.local[key] || state.user?.global[key];
+    }    
 
     return <div className="p-2 h-100 w-100">
+        <div className="d-flex align-items-center justify-content-end">
+            <span className="">Show global configs</span>
+            <span className="ps-2">
+                <Form.Switch checked={state.showingGlobal} onChange={_=> setState({showingGlobal:!state.showingGlobal})} />
+            </span>
+        </div>
         <div className="d-flex config-item">
-            <span className="config-header" style={{width:`${headerWidth}px`}}>
+            <span className="config-header">
                 User name:
             </span>
-            <span className="config-value">{state.name.value}</span>
+            <span className="config-value">
+                <span>{getValue("name")}</span>
+            </span>
         </div>
         <div className="d-flex config-item">
             <span className="config-header">
                 Email:
             </span>
-            <span className="config-value">{state.email.value}</span>
-        </div>
-        <div>
+            <span className="config-value">
+                <span>
+                    {getValue("email")}
+                </span>
 
+            </span>
         </div>
     </div>
 }
