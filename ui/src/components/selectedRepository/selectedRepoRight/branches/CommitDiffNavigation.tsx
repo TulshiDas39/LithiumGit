2d950a306c399bdf45c57cc907523c67361de70a
tulshi das <tulshidas37@gmail.com>
2024-12-22 22:44:15 +0600

    handle text overflow

--- ui/src/components/selectedRepository/selectedRepoRight/branches/CommitDiffNavigation.tsx
+++ ui/src/components/selectedRepository/selectedRepoRight/branches/CommitDiffNavigation.tsx
@@ -33,7 +33,7 @@ function CommitDiffNavigationComponent(props:IProps){
     return <div className="d-flex">
         <div className="flex-grow-1 d-flex align-items-center justify-content-center">
             {!!props.file && <Fragment>
-                <span className="pe-1" title={props.file.path}>{StringUtils.getFileName(props.file.path)} </span>
+                <span className="pe-1 overflow-ellipsis" title={props.file.path} style={{maxWidth:300}}>{StringUtils.getFileName(props.file.path)} </span>
                 <span>({hash1}</span>
                 {!!hash2 && 
                    <Fragment>
