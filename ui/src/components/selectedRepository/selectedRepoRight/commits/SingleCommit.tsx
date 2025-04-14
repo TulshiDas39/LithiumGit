3a15a4b09d76bfef6e88528d2422b9c69206d3f6
tulshi das <tulshidas37@gmail.com>
2025-04-14 22:57:18 +0600

    show dropdown

--- ui/src/components/selectedRepository/selectedRepoRight/commits/SingleCommit.tsx
+++ ui/src/components/selectedRepository/selectedRepoRight/commits/SingleCommit.tsx
@@ -1,9 +1,10 @@
 import { ICommitInfo } from "common_library";
-import { UiUtils } from "../../../../lib";
+import { UiUtils, useMultiState } from "../../../../lib";
 import moment from "moment";
 import React, { useRef } from "react";
 import { FaCircle, FaDotCircle, FaEllipsisV, FaHashtag, FaKey, FaKeybase, FaKeycdn, FaUser } from "react-icons/fa";
 import { ModalData } from "../../../modals/ModalData";
+import { Dropdown } from "react-bootstrap";
 
 interface ISingleCommitProps{
     commit:ICommitInfo;
@@ -15,15 +16,21 @@ interface ISingleCommitProps{
 interface IRefData{
     hoverElipsis:boolean;
 }
+
+interface IState{
+    showDropdown:boolean;
+}
 function SingleCommitComponent(props:ISingleCommitProps){
+    const [state,setState] = useMultiState<IState>({showDropdown:false});
     const refData = useRef<IRefData>({hoverElipsis:false});
     const getTimeZonOffsetStr = ()=>{
         return UiUtils.getTimeZonOffsetStr();
     }
     const handleEllipsisClick = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>)=>{
         //e.preventDefault(); 
-        e.stopPropagation();        
-        props.onRightClick(e as any,props.commit);
+        //e.stopPropagation();        
+        //props.onRightClick(e as any,props.commit);
+        //setState({showDropdown:!state.showDropdown});
         // ModalData.commitContextModal.selectedCommit=props.commit!;            
         // ModalData.commitContextModal.position = {
         //     x:e.clientX,
@@ -36,7 +43,12 @@ function SingleCommitComponent(props:ISingleCommitProps){
             return ;
         props.onSelect(props.commit);
     }
-    return <div className={`py-1 w-100 overflow-auto ${props.isSelected?'selected':''} ${props.highlighted?'highlighted':''}`} onClick={()=>handleDivClick()} onContextMenu={e=>props.onRightClick(e,props.commit)}>
+    const handleContext = (e: React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
+        if(refData.current.hoverElipsis)
+            return ;
+        props.onRightClick(e,props.commit);
+    }
+    return <div className={`py-1 w-100 gs-overflow-x-auto ${props.isSelected?'selected':''} ${props.highlighted?'highlighted':''}`} onClick={()=>handleDivClick()} onContextMenu={handleContext}>
      <div className="border border-secondary ps-2">
         <div className="d-flex">
             <div className="flex-grow-1">
@@ -44,6 +56,17 @@ function SingleCommitComponent(props:ISingleCommitProps){
                 <span>{props.commit.hash}</span>
                 {!!props.commit.refs && 
                 <b className="text-danger"> ({props.commit.refs})</b>}
+            </div>
+            <div className="d-flex justify-content-end align-items-center">            
+                <Dropdown autoClose="outside" className="pe-2">
+                    <Dropdown.Toggle variant="link" id="dropdown-commit-list-item" className="rounded-0 no-caret"
+                        onMouseEnter={()=> refData.current.hoverElipsis = true} onMouseLeave={()=> refData.current.hoverElipsis = false}>
+                        <FaEllipsisV />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="no-radius">
+                        <Dropdown.Item onClick={()=>{}} className="border-bottom">Show in graph</Dropdown.Item>                                
+                    </Dropdown.Menu>
+                </Dropdown>                        
             </div>
             
              {/* <span className="hover" onMouseEnter={()=> refData.current.hoverElipsis = true} 
