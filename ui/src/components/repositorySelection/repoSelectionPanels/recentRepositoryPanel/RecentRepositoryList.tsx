a1a6ca376b90c1759f13743d33f59082517d7f7d
tulshi das <tulshidas37@gmail.com>
2025-11-29 01:35:08 +0600

    handle hover effect

--- ui/src/components/repositorySelection/repoSelectionPanels/recentRepositoryPanel/RecentRepositoryList.tsx
+++ ui/src/components/repositorySelection/repoSelectionPanels/recentRepositoryPanel/RecentRepositoryList.tsx
@@ -6,16 +6,23 @@ import { ActionModals, ActionSavedData } from "../../../../store/slices";
 import { IpcUtils } from "../../../../lib/utils/IpcUtils";
 import { ModalData } from "../../../modals/ModalData";
 import { EnumModals } from "../../../../lib";
+import { FaTrashAlt } from "react-icons/fa";
 
 export interface IRecentRepositoryListProps{
     onSelectItem:(item:RepositoryInfo)=>void;
     selectedItem?:RepositoryInfo;
 }
 
+interface IState{
+    hoverRepoId:string;
+}
+
 function RecentRepositoryListComponent(props:IRecentRepositoryListProps){
     const store = useSelectorTyped(state=>({
         recentRepos:state.savedData.recentRepositories,
     }),shallowEqual);
+
+    const [state, setState] = React.useState<IState>({ hoverRepoId: "" });
 
     const dispatch = useDispatch();
 
@@ -36,6 +43,17 @@ function RecentRepositoryListComponent(props:IRecentRepositoryListProps){
             dispatch(ActionSavedData.setSelectedRepository(item));
         }
     }
+
+    const handleDelete=(e:React.MouseEvent,repo:RepositoryInfo)=>{
+        e.stopPropagation();        
+
+        ModalData.confirmationModal.message = "Are you sure you want to remove this repository from recent list?";
+        ModalData.confirmationModal.YesHandler = ()=>{
+            dispatch(ActionSavedData.removeRepositoryFromRecentList(repo));
+        }
+        dispatch(ActionModals.showModal(EnumModals.CONFIRMATION));
+    }
+
     return <div id="recentRepoList" className="w-75 h-100 d-flex flex-column">
         <h4 className="px-1 py-2 m-0">Recent Repositories</h4>
         <hr className="m-0" />
@@ -43,10 +61,13 @@ function RecentRepositoryListComponent(props:IRecentRepositoryListProps){
             {
                 store.recentRepos.map(repo=>(
                     <div key={repo._id} className={`repoItem hover border-bottom pt-2 ${props.selectedItem?._id === repo._id?"selected":""}`}                        
-                        onClick={()=>handleSelect(repo)}>
+                        onClick={()=>handleSelect(repo)} onMouseEnter={() => setState({hoverRepoId:repo._id})} onMouseLeave={() => setState({hoverRepoId: ""})}>
                         <div className="d-flex flex-column px-1 w-100" onDoubleClick={()=>handleDoubleClick(repo)}>
-                            <h6>{repo.name}</h6>
-                            <span>{repo.path}</span>
+                            <div className="d-flex">
+                                <h6 className="flex-grow-1 overflow-ellipsis">{repo.name}</h6>
+                                {state.hoverRepoId === repo._id && <span className="small"><FaTrashAlt title="Delete" className="hover-danger" onClick={(e) => handleDelete(e,repo)} /></span>}
+                            </div>
+                            <span className="overflow-ellipsis">{repo.path}</span>
                         </div>                        
                     </div>
                 ))
