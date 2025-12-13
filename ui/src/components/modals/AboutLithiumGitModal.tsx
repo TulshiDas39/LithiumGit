632d4679de93d9013fe51528c418e095469ecb79
tulshi das <tulshidas37@gmail.com>
2025-12-14 00:32:09 +0600

    show about info

--- ui/src/components/modals/AboutLithiumGitModal.tsx
+++ ui/src/components/modals/AboutLithiumGitModal.tsx
@@ -1,10 +1,21 @@
 import { Modal } from "react-bootstrap";
-import { EnumModals } from "../../lib";
+import { EnumModals, UiUtils, useMultiState } from "../../lib";
 import { useSelectorTyped } from "../../store/rootReducer";
 import { shallowEqual, useDispatch } from "react-redux";
 import { ActionModals } from "../../store";
-import { FaTimes } from "react-icons/fa";
+import { FaCopy, FaTimes } from "react-icons/fa";
 import React from "react";
+import { ModalData } from "./ModalData";
+
+
+enum AboutTabs{
+    ABOUT="ABOUT",
+    WHATS_NEW="WHATS_NEW",
+}
+
+interface IState{
+    activeTab:AboutTabs;
+}
 
 function AboutLithiumGitModalComponent() {
     const store = useSelectorTyped(state => ({
@@ -12,6 +23,9 @@ function AboutLithiumGitModalComponent() {
     }), shallowEqual);
 
     const dispatch = useDispatch();
+    const [state,setState] = useMultiState<IState>({
+        activeTab: AboutTabs.ABOUT,
+    });
 
     return <Modal show={store.show} dialogClassName="" 
         backdropClassName="bg-transparent" animation={false} size="lg" >
@@ -26,11 +40,14 @@ function AboutLithiumGitModalComponent() {
             </div>
             <div className="row g-0 py-3 align-items-center">
                 <div className="col-2">
-                    <div className="py-2 ps-2 pe-4 bg-third-color border-bottom cur-default hover">About</div>
-                    <div className="py-2 ps-2 pe-4 bg-third-color cur-default hover">What's new?</div>
+                    <div className={`py-2 ps-2 pe-4 bg-third-color border-bottom cur-default hover ${state.activeTab === AboutTabs.ABOUT ? "selected" : ""}`} 
+                    onClick={()=>setState({activeTab:AboutTabs.ABOUT})}>About</div>
+                    <div className={`py-2 ps-2 pe-4 bg-third-color cur-default hover ${state.activeTab === AboutTabs.WHATS_NEW ? "selected" : ""}`}
+                    onClick={()=>setState({activeTab:AboutTabs.WHATS_NEW})}>What's new?</div>
                 </div>
                 <div className="col-10">
-                    
+                    {state.activeTab === AboutTabs.ABOUT && <AboutInfo />}
+                    {state.activeTab === AboutTabs.WHATS_NEW && <NewFeatures />}
                 </div>
             </div>           
         </Modal.Body>
@@ -38,3 +55,36 @@ function AboutLithiumGitModalComponent() {
 }
 
 export const AboutLithiumGitModal = React.memo(AboutLithiumGitModalComponent);
+
+
+function AboutInfoComponent() {
+    const dispatch = useDispatch();
+
+    const handleSiteCopy = () => {
+        UiUtils.copy("https://lithiumgit.com");
+        ModalData.appToast.message = "Website URL copied to clipboard";
+        dispatch(ActionModals.showToast());
+    }
+    return <div>
+        <h4 className="text-center">About LithiumGit</h4>
+        <div className="ps-4">
+            <div>Product Name: LithiumGit</div>
+            <div>Version: 1.0.7</div>
+            <div>Website: https://lithiumgit.com  <span className="small hover-color cur-point overflow-ellipsis" onClick={()=> handleSiteCopy()}><FaCopy className="click-effect" /></span> </div>
+        </div>
+    </div>
+}
+
+export const AboutInfo = React.memo(AboutInfoComponent);
+
+function NewFeaturesComponent() {
+    return <div>
+        <div>
+            Whats new in LithiumGit version 1.0.7:
+        </div>
+        <div className="pt-2">
+        </div>
+    </div> 
+}
+
+export const NewFeatures = React.memo(NewFeaturesComponent);
