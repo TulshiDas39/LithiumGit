d5101961b82290eafcaf10e30d9f930535919d31
tulshi das <tulshidas37@gmail.com>
2025-12-14 17:31:19 +0600

    complete about info

--- ui/src/components/modals/AboutLithiumGitModal.tsx
+++ ui/src/components/modals/AboutLithiumGitModal.tsx
@@ -7,6 +7,7 @@ import { FaCopy, FaTimes } from "react-icons/fa";
 import React from "react";
 import { ModalData } from "./ModalData";
 import moment from "moment";
+import { IpcUtils } from "../../lib/utils/IpcUtils";
 
 
 enum AboutTabs{
@@ -41,7 +42,7 @@ function AboutLithiumGitModalComponent() {
                     <span className="hover" onClick={_=> dispatch(ActionModals.hideModal(EnumModals.ABOUT_LITHIUMGIT))}><FaTimes className="" /></span>
                 </div>
             </div>
-            <div className="row g-0 py-3 align-items-center">
+            <div className="row g-0 py-3">
                 <div className="col-2">
                     <div className={`py-2 ps-2 pe-4 bg-third-color border-bottom cur-default hover ${state.activeTab === AboutTabs.ABOUT ? "selected" : ""}`} 
                     onClick={()=>setState({activeTab:AboutTabs.ABOUT})}>About</div>
@@ -72,13 +73,36 @@ function AboutInfoComponent() {
         ModalData.appToast.message = "Website URL copied to clipboard";
         dispatch(ActionModals.showToast());
     }
+    const openWebsite = () => {
+        IpcUtils.openLink("https://lithiumgit.com");
+    }
+
+    const openGitHub = () => {
+        IpcUtils.openLink("https://github.com/tulshidas39/lithiumgit");
+    }
+
+    const openDocumentation = () => {
+        IpcUtils.openLink("https://lithiumgit.com/docs");
+    }
+
+    const openIssueTracker = () => {
+        IpcUtils.openLink("https://github.com/tulshidas39/lithiumgit/issues")
+    }
+
     return <div>
         <h4 className="text-center">About LithiumGit</h4>
         <div className="ps-4">
             <div>Product Name: LithiumGit</div>
             <div>Version: {store.version}</div>
             <div>Release date:  {moment(store.releaseDate).format('MMMM DD, YYYY')}</div>
-            <div>Website: https://lithiumgit.com  <span className="small hover-color cur-point overflow-ellipsis" onClick={()=> handleSiteCopy()}><FaCopy className="click-effect" /></span> </div>
+            <div>Website: <span className="hover-color cur-point" onClick={openWebsite}>https://lithiumgit.com</span> <span className="small hover-color cur-point overflow-ellipsis" onClick={()=> handleSiteCopy()}><FaCopy className="click-effect" /></span> </div>
+            <div>Software Category: Open Source Software</div>            
+            <div>License: MIT License</div>     
+            <div>Github: <span className="hover-color cur-point" onClick={openGitHub}>https://github.com/tulshidas39/lithiumgit</span></div>
+            <div>Documentation: <span className="hover-color cur-point" onClick={openDocumentation}>https://lithiumgit.com/docs</span></div>
+            <div>Issue Tracker: <span className="hover-color cur-point" onClick={openIssueTracker}>https://github.com/tulshidas39/lithiumgit/issues</span></div>
+            
+            <div>Copyright: &copy;2020-Present LithiumGit community</div>
         </div>
     </div>
 }
