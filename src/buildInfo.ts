4b7847a3cb79775ff005941fc69e59089dabfef7
tulshi das <tulshidas37@gmail.com>
2025-12-26 01:00:54 +0600

    new release

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,5 +1,5 @@
 // ⚠️ This file is auto-generated. Do not edit manually.
 export const BUILD_INFO = {
-  timestamp: "2025-12-14T15:42:25.030Z",
-  version: "1.0.7"
+  timestamp: "2025-12-24T17:32:53.336Z",
+  version: "1.0.8"
 } as const;
