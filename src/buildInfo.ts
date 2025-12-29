75f1c590e046eca9c0180f5564529c1e00974caf
tulshi das <tulshidas37@gmail.com>
2025-12-30 00:07:35 +0600

    add build info

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,5 +1,5 @@
 // ⚠️ This file is auto-generated. Do not edit manually.
 export const BUILD_INFO = {
-  timestamp: "2025-12-24T17:32:53.336Z",
+  timestamp: "2025-12-27T16:11:41.252Z",
   version: "1.0.8"
 } as const;
