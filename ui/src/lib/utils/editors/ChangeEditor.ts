210dae18ac64af0f5e3679fbdd2abd6b54a0b122
tulshi das <tulshidas37@gmail.com>
2026-03-29 21:55:10 +0600

    remove unused code

--- ui/src/lib/utils/editors/ChangeEditor.ts
+++ ui/src/lib/utils/editors/ChangeEditor.ts
@@ -27,14 +27,12 @@ export class ChangeEditor extends TextEditor{
                 const iline = this._ilines[index];
                 if(iline.hightLightBackground){
                     decorations.push(Decoration.node(offset, offset + node.nodeSize, { class: 'bg-current-change' }));
-                    // node.forEach((_child, _childOffset) => {                      
-                        for(let i=0; i<iline.textHightlightIndex.length; i++){
-                            const range = iline.textHightlightIndex[i];
-                            const start = offset + 1 + range.fromIndex;
-                            const end = start + range.count;
-                            decorations.push(Decoration.inline(start, end, { class: 'bg-current-change-deep h-100' }));
-                        }
-                    // });
+                    for(let i=0; i<iline.textHightlightIndex.length; i++){
+                        const range = iline.textHightlightIndex[i];
+                        const start = offset + 1 + range.fromIndex;
+                        const end = start + range.count;
+                        decorations.push(Decoration.inline(start, end, { class: 'bg-current-change-deep' }));
+                    }
                 }
             });
             return DecorationSet.create(doc, decorations);
