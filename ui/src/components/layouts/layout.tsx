e95ffc7980a3d92dfb3d685549f67f5fa9594b66
tulshi das <tulshidas37@gmail.com>
2026-06-05 00:05:27 +0600

    remove unused code

--- ui/src/components/layouts/layout.tsx
+++ ui/src/components/layouts/layout.tsx
@@ -9,20 +9,7 @@ interface ILayoutProps{
     height:number
 }
 
-function LayoutComponent(props:ILayoutProps) {
-    const data = useRef({topHeighPercent:7,bottomNavHeightPercent:3});
-
-    const topNavHeight = useMemo(()=>{
-        const height = props.height * (data.current.topHeighPercent/100);
-        if(height > 40) return 40;
-        return height;
-    },[props.height]);
-
-    const footerHeight = useMemo(()=>{
-        const height = props.height * (data.current.bottomNavHeightPercent/100);
-        if(height > 20) return 20;
-        return height;
-    },[props.height])    
+function LayoutComponent(props:ILayoutProps) {    
 
     return (
         <div className="h-100" style={{height:props.height+"px", position: "relative"}}>
