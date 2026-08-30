240517d5f8a812bca9507e7b018bbc10355254b4
tulshi das <tulshidas37@gmail.com>
2026-08-30 18:03:18 +0600

    clean up code

--- ui/src/components/selectedRepository/selectedRepoRight/changes/ConflictView.tsx
+++ ui/src/components/selectedRepository/selectedRepoRight/changes/ConflictView.tsx
@@ -1,5 +1,4 @@
-import { useMemo, useRef } from "react"
-import { EnumHtmlIds, ILine, useDrag } from "../../../../lib"
+import { EnumHtmlIds, ILine } from "../../../../lib"
 import { ConflictTopPanel } from "./ConflictTopPanel";
 
 interface IProps{
@@ -8,28 +7,6 @@ interface IProps{
 }
 
 export function ConflictView(props:IProps){
-    const hightDisplacementRef = 0;//useRef(0);
-    const hightDisplacement = 0;
-    // const positionRef = useRef(0);
-    // const {currentMousePosition:position,elementRef:resizer} = useDrag();
-
-    // const hightDisplacement = useMemo(()=>{
-    //     return 0;
-    //     if(!position){
-    //         hightDisplacementRef.current -= positionRef.current;
-    //         positionRef.current = 0;
-    //         return hightDisplacementRef.current;
-    //     }
-    //     positionRef.current = position.y;
-    //     return hightDisplacementRef.current - positionRef.current;
-    // },[position?.y])
-
-
-    const getSign=(value:number)=>{
-        if(value < 0)
-            return "-";
-        return "+";
-    }
 
     return <div id="conflict-editor"  className="h-100 w-100">
         <div style={{height:30}} className="d-flex align-items-center w-100 border-bottom">
@@ -47,12 +24,12 @@ export function ConflictView(props:IProps){
             </div>
         </div>
         <div style={{height:'calc(100% - 33px)'}}>
-            <div className="w-100 top-diff" id={EnumHtmlIds.ConflictEditorTopPanel} style={{height:`calc(50% ${getSign(-(0+3))}  ${Math.abs(hightDisplacement+3)}px)`}}>
+            <div className="w-100 top-diff" id={EnumHtmlIds.ConflictEditorTopPanel} style={{height:`calc(50% - 3px)`}}>
                 <ConflictTopPanel currentLines={props.currentLines}
                     previousLines={props.incomingLines} />
             </div>
             <div className="w-100 bg-second-color resizer cur-resize-v" style={{height:3}}/>
-            <div className="w-100" id={EnumHtmlIds.ConflictEditorBottomPanel} style={{height:`calc(50% ${getSign(0)} ${Math.abs(0)}px)`}}>
+            <div className="w-100" id={EnumHtmlIds.ConflictEditorBottomPanel} style={{height:`50%`}}>
                 <div className="h-100 w-100 d-flex conflict-bottom">
                     <div className="noselect line_numbers overflow-y-hidden h-100"></div>
                     <div className="h-100 content-container overflow-auto flex-grow-1">
