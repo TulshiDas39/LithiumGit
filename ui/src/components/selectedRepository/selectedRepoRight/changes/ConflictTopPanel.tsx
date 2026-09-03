c81b5e30cf3246a252d696aa178781b3e2abe550
Tulshi Das <tulshidas37@gmail.com>
2026-09-03 13:40:13 +0800

    update top label checkbox state

--- ui/src/components/selectedRepository/selectedRepoRight/changes/ConflictTopPanel.tsx
+++ ui/src/components/selectedRepository/selectedRepoRight/changes/ConflictTopPanel.tsx
@@ -1,23 +1,28 @@
 import { EnumConflictSide } from "common_library";
-import { EnumHtmlIds, IConflictLine, ILine } from "../../../../lib"
+import { EnumHtmlIds, IConflictLine } from "../../../../lib"
 import { ConflictDiffView } from "./ConflictDiffView";
 
 interface IProps{
     previousLines:IConflictLine[];
     currentLines:IConflictLine[];
 }
+
 export function ConflictTopPanel(props:IProps){    
+
+    const topLabelInCbChecked = props.previousLines.filter(x => !!x.conflictNo).every(x=> !!x.taken);
+    const topLabelCurCbChecked = props.currentLines.filter(x => !!x.conflictNo).every(x=> !!x.taken);
+    
     return <div className="h-100">
             <div style={{height:30}} className="d-flex align-items-center w-100 border-bottom">
                 <div className={"w-50 d-flex align-items-center"}>
                     <div className="check_all_incoming d-flex justify-content-end">
-                        <input id={EnumHtmlIds.accept_all_incoming} type="checkbox" title="Accept all incoming changes" />
+                        <input id={EnumHtmlIds.accept_all_incoming} type="checkbox" title="Accept all incoming changes" checked={topLabelInCbChecked} />
                     </div>
                     <div className="ps-2">Incoming changes</div>
                 </div>
                 <div className="w-50 d-flex align-items-center">
                     <div className="check_all_current d-flex justify-content-end">
-                        <input id={EnumHtmlIds.accept_all_current} type="checkbox" title="Accept all current changes" />
+                        <input id={EnumHtmlIds.accept_all_current} type="checkbox" title="Accept all current changes" checked={topLabelCurCbChecked} />
                     </div>                
                     <div className="ps-2">Current changes</div>
                 </div>
